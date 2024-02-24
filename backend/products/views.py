979f4868df22346ad1e8bfe4596f3799af9fe68b
H3L70N <heltonnguammbe@gmail.com>
2024-02-24 15:27:19 +0200

    add authentication

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,14 +1,19 @@
-from rest_framework import generics, mixins
+from rest_framework import authentication, generics, mixins, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 from .models import Product
 from .serializers import ProductSerializer
-
+from .permissions import IsStaffEditorPermission
+# noinspection PyUnresolvedReferences
+from api.authentication import TokenAuthentication
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication, TokenAuthentication]
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
+
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
--- py_client/list.py
+++ py_client/list.py
@@ -1,11 +1,26 @@
 import requests
-
-endpoint = "http://localhost:8000/api/products/"
-
-response = requests.get(endpoint)
+from getpass import getpass
 
 
-# print(response.headers)
-# print(response.text)
-print(response.json())
-print(response.status_code)
+auth_endpoint = "http://localhost:8000/api/auth/"
+username = input("Username: ")
+password = getpass("Password: ")
+
+auth_response = requests.post(auth_endpoint, json={'username': username, 'password': password})
+print(auth_response.json())
+
+
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    endpoint = "http://localhost:8000/api/products/"
+
+    response = requests.get(endpoint, headers=headers)
+
+
+    # print(response.headers)
+    # print(response.text)
+    print(response.json())
+    print(response.status_code)
